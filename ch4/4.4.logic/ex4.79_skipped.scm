6675bc5ddd29fd4619df5b9b20478de84907007d
Laurent Direr <4263535+ldirer@users.noreply.github.com>
2025-01-08 18:56:26 +0100

    skipping 4.79... for now?

---
+++ ch4/4.4.logic/ex4.79_skipped.scm
@@ -0,0 +1,3 @@
+; The last part of the exercise... seems complicated :)
+;https://web.archive.org/web/20230321170331/https://community.schemewiki.org/?sicp-ex-4.79
+; Implementing a version that works using environments should be manageable though.
